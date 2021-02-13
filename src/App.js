f8976d8a4ed384ad8d8343da0857f56d70cd0d8f
Elijah <elijahoneal@yahoo.com>
2021-02-13 00:10:42 -0500

    Form component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import Form from './Form'
+
+
+const initialState = { name: '', email: '', role: ''}
 
 function App() {
+  const [team , setTeam] = useState([])
+  const [form , setForm] = useState({})
+  useEffect( ()=> {
+    setTeam([{ name: 'Eli', email: 'elioneal@gmail.com', role: 'frontend' }])
+  },[] )
+
+const submit = () => {
+  setTeam([...team , form])
+  setForm(initialState)
+}
+
+const update = (name , value) => {
+  setForm({...form , [name]: value})
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form  form={form} submit={submit} update={update}/>
+
+      { team.map( member => {
+        return (
+          <div>
+            <h2>{member.name}</h2>
+            <p>{member.email}</p>
+            <p>{member.role}</p>
+          </div>
+        )
+      } ) }
     </div>
   );
 }
